5cb30c8167f3c0bae6dd21a169e0c65052c81a34
Phil S <philipstubbs13@gmail.com>
2017-12-07 09:23:07 -0600

    updated comments in code to reflect latest code updates. Fixed issue where guesses counter was decreasing for already guessed letters by adding return statement to function.

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -7,16 +7,16 @@
 	//Computer chooses random word from list
 	var randomWord = "";
 
-	//Creating an empty array that will hold all the letters in the random word that the computer generates.
+	//Create an empty array that will hold all the letters in the random word that the computer generates.
 	var lettersInWord = [];
 
 	//Number of underscores needed for random word.
 	var numberUnderscoresNeeded = 0;
 
-	//Creating an empty array, which will be used to replace the underscores with successful guesses by the user.
+	//Create an empty array, which will be used to replace the underscores with successful guesses by the user.
 	var underscoresSuccesses = [];
 
-	//Creating an empty array to hold all the letters not in a word or that were already guessed.
+	//Create an empty array to hold all the letters not in the random word or that were already guessed.
 	var lettersNotInWord = [""]; 
 	var lettersNotInWordList = [ ];
 
@@ -49,13 +49,13 @@ function start () {
 	console.log(numberUnderscoresNeeded);
 
 	//Reset
-	guessesRemainder = 14;
+	guessesRemainder = 13;
 	lettersNotInWord = 0;
 	underscoresSuccesses = [];
 	lettersNotInWordList = [ ];
 	lettersNotInWord = [""];
 
-	//Problem: After the app generates a random word, we need a way to add the appropriate number of underscores("_") to the Current Word section
+	//Problem: After the computer generates a random word, need a way to add the appropriate number of underscores("_") to the Current Word section
 	//based on the number of letters in the word.
 	//Solution: Create for loop that loops through numberUnderscoresNeeded, which equals lettersWord.length.
 	for (var i=0; i < numberUnderscoresNeeded; i++ ) {
@@ -84,7 +84,7 @@ function start () {
 	
 			//Need to determine whether the user pressed a letter, number, or anything else.
 			//https://stackoverflow.com/questions/34687895/determine-if-a-letter-or-a-number-was-pressed-javascript
-			//userGuess is our keyCode. Let's store the userGuess variable as a string. Also, if user has caps lock on, let's convert the letter from upper case to lower case.
+			//userGuess is our keyCode. Store the userGuess variable as a string. Also, if user has caps lock on, convert the letter from upper case to lower case.
 			var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 			//If user pressed a letter, game starts (check letter against the letters in the randowm word).
@@ -101,8 +101,10 @@ function start () {
 function checkLetters(userGuess) {
 	//I want to check if the letter the user guesses exists in the word.
 	//Need to find out if letter was already guessed by the user. If already guessed by the user, notify the user to select another letter.
+	//User shouldn't be able to continue with game if they guess the same letter more than once.
 	if (lettersNotInWordList.indexOf(userGuess) > -1) {
 		alert("You already guessed that letter. Pick another letter.")
+		return;
 		}
 
 	//If user did not already pick letter from a previous guess, push letter to the lettersNotInWordList array.
@@ -120,25 +122,21 @@ function checkLetters(userGuess) {
 	for (var i = 0; i < numberUnderscoresNeeded; i++){
 		//If letter that the user guessed was found in the random word....
     	if (userGuess === lettersInWord[i]) {
-    	//if (lettersInWord[i] > -1) {
         console.log("letter found");
         var isletterInWord = true;
 		underscoresSuccesses[i] = userGuess;
 		console.log(underscoresSuccesses[i]);
 		console.log(lettersInWord[i]);
-		//document.getElementById("underscore").innerHTML = underscoresSuccesses.join("");
 		}
 
-		//Else, if letter that the user guessed was not found in the random word...
+		//Else, if letter that the user guessed was not found in the random word, isletterInWord is false.
 		//Note that we have already added the guessed word to the lettersNotInWordList.
-		//if (randomWord.indexOf(userGuess) === -1 && alphabet.indexOf(userGuess) > -1 && userGuess != lettersInWord[i]) {
-		//else if (lettersNotInWordList.indexOf(userGuess) === -1) {
 		else if (lettersInWord[i] == -1){
 			isletterInWord = false;
 			}
 	}
 
-	//If the letter is in the word, let's check where in the word the letter exists, and then populate the underscoresSuccesses array in our HTML.
+	//If the letter is in the word, check where in the word the letter exists, and then populate the underscoresSuccesses array in the HTML.
 	if (isletterInWord) {
 		 for (var i = 0; i < numberUnderscoresNeeded; i++) {
 			if (lettersInWord[i] == userGuess) {
@@ -157,8 +155,6 @@ function checkLetters(userGuess) {
 		$("#guessesLeft-Header").html("<p>Number of guesses remaining</p>");
 		//Write number of guesses to div in html with id = guessesLeft.
 		$("#guessesLeft").html(guessesRemainder);
-		//document.getElementById("LettersGuessed").innerHTML = lettersNotInWord.join(" ");
-		//document.getElementById("guessesLeft").innerHTML = guessesRemainder;
 		//console.log(guessesRemainder);
 	}
 
@@ -179,22 +175,15 @@ function roundComplete() {
 	//log the wins, losses, and remaining guesses counts to the console.
 	console.log("Win Count: " + wins + " | loss Count: "+ losses + " | Guesses Left: " + guessesRemainder);
 
-	//I need to update the HTML with guesses remaining, underscores and successes, and letters already guessed.
-	//document.getElementById("guessesLeft").innerHTML = guessesRemainder;
-	//document.getElementById("underscore").innerHTML = underscoresSuccesses.join("");
-	//document.getElementById("lettersGuessed").innerHTML = lettersNotInWord.join("");
-	//document.getElementById("lettersGuessed").innerHTML = lettersNotInWord.join;
-	//console.log(lettersNotInWord.join);
-
 	//I want to check if the user won. If the user won, the letters in the lettersInWord array are the same as the letters in the underscoreSuccessarray.
-	//Convert the letters in both arrays to compare.
+	//Convert the letters to strings in both arrays to compare.
 	if (lettersInWord.toString() == underscoresSuccesses.toString()) {
 		wins++;
 		//Add to the number of wins in the HTML.
 		$(".win-counter").html(wins);
 		alert("You won");
 
-		//Ask the user if they want to play the game. If user clicks OK, reset game and choose new word.
+		//Create a confirm dialog box to ask the user if they want to play the game. If user clicks OK, reset game and choose new word.
 		//If user clicks Cancel, go back to main home screen.
 		var playAgain = confirm('Do you want to play again?');
 		if (playAgain) {
@@ -206,14 +195,14 @@ function roundComplete() {
 		}
 	}
 
-	//Check if user lost
+	//Check if user lost. If remaining guesses gets down to zero, user lost.
 	else if (guessesRemainder === 0){
 		losses++;
 		//Add to the number of losses in the HTML.
 		$(".loss-counter").html(losses);
 		alert("You lost");
 
-		//Ask the user if they want to play the game again. If user clicks OK, reset game and choose new word.
+		//Create a confirm dialog box to ask the user if they want to play the game again. If user clicks OK, reset game and choose new word.
 		//If user clicks Cancel, go back to main home screen.
 		var playAgain = confirm('Do you want to play again?');
 		if (playAgain) {
@@ -227,13 +216,13 @@ function roundComplete() {
 	
 }
 
-//Process - Execute the code. Do not place anything else below this line.
+//Execute the code. Do not place anything else below this line.
 //===================================================================
 
 //Starts/resets the code/game
 start();
 
-//check/compare the letter that the user guessed to the letters in the word.
+//check and compare the letter that the user guessed to the letters in the random word generated by the computer..
 //If the letter is in the word, populate the letter in the HTML.
 checkLetters (userGuess);
 
